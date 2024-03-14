538b5901f3da660b4b580f3ba6ea56c708444127
enzohasbram <enzohbrambila@gmail.com>
2024-03-14 16:13:09 -0300

    Module Review6

---
+++ review6.py
@@ -0,0 +1,13 @@
+def msg():
+    texto = str(input("Digite uma mensagem"))
+    return texto
+
+def flt():
+    numero = float(input("Digite um número"))
+    return numero
+
+def ler_msg(mensagem):
+    return input(mensagem)
+
+def ler_flt(mensagem):
+    return input(mensagem)
